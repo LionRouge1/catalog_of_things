30cfd7774680332e956d20e6d2688d64148c7730
Masoudi Parrot <masoudi@localhost.localdomain>
2022-07-04 13:06:19 -0400

    main page for the application

---
+++ main.rb
@@ -0,0 +1,65 @@
+require './app'
+require './actions/movie_actions'
+
+@my_app = App.new
+
+def menu
+  puts 'WELCOME TO CATALOG OF THINGS'
+  puts ''
+  puts 'Please choose an option by entering a number:'
+  puts '1 - List all books'
+  puts '2 - List all music albums'
+  puts '3 - List all movies'
+  puts '4 - List all games'
+  puts '5 - List all genres'
+  puts '6 - List all labels'
+  puts '7 - List all authors'
+  puts '8 - List all sources'
+  puts '9 - Add a book'
+  puts '10 - Add music album'
+  puts '11 - Add a movie'
+  puts '12 - Add a games'
+  puts '13 - Exit'
+end
+
+def main
+  menu
+  option = gets.chomp.to_i
+  select_option(option)
+end
+
+def middle
+  puts 'Select (0) to goback to the menu or (13) to exit'
+  mid = gets.chomp.to_i
+  case mid
+  when 0
+    main
+  when 13
+    select_option(13)
+  else
+    p 'Invalid input'
+    main
+  end
+end
+
+def select_option(option)
+  case option
+
+  when 1..12
+    @my_app.redirection(option)
+    middle
+  # when 10..12
+  #   puts 'On construction'
+  #   print "\e[2J\e[f"
+  #   main
+  when 13
+    puts 'Goodbye see you again'
+    @my_app.redirection(option)
+    exit(true)
+  else
+    puts 'Choose a correct option'
+    main
+  end
+end
+
+main
